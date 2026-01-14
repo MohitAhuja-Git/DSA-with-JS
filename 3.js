f84032d511a41045123662c16383fa4f71d242de
Laptop <ma8626564@gmail.com>
2026-01-14 09:21:37 +0530

    video 3 compleate mostly

---
+++ 3.js
@@ -0,0 +1,80 @@
+// -----------------------IMP-----------------------------|
+// Var :- Variable that can use outside the Block/Brackets
+// Let :- Variable that can use Inside The Block/Brackets
+// const :- Constant that can use Everyware in Code.
+// Break :- Break and stop the loop after any Given Conditioin.
+// Continue :- Skip Loop To next itration[Value of i is skip].
+
+// Loops-------------------------------------------------------
+
+// for(start; End; Change;){Code.. }---Number of itration pta hoti h
+
+// Start;
+// while (End) {Change; Code.. }---Number of itration ka nhi pta - [start ka nhi pta per end ka pta h]
+
+// do-while :-ek bar to code chale ga without any condition
+// do{}
+
+
+
+// Questions-----------------------------------
+// let a = Number(prompt("Enter a Number"));
+
+// if (isNaN(a)) {
+//     console.log("Failled!!");
+// }
+// else if (a > 0) {
+
+// }
+// else {
+//     console.log("Should Be +ve and More than zero");
+// }
+
+
+// for (let i = 1; i==1 ;) {
+//     // let c=prompt("Hello");
+//     // confirm("fhasdgdjklshfgsljkfhasdjklf")
+//     alert("Lolu")
+//     console.log(c)
+// }
+
+// let a = (prompt("Enter Stop To Stop"));
+
+// do{
+
+//     console.log("Hello")
+// } while (a!=="stop") {
+//         console.log("Hello")    
+//     }
+
+// Game
+
+let c = confirm("Do You Wants To Play A Number Guessing Game?");
+if (c == true) {
+    alert("Game is start in Console , Tap Ok For Play")
+    let a = Math.trunc(Math.random() * 10)
+    console.log("Computer Chose A Number B/W 0 To 9")
+    console.log("Now You Guess!")
+    var ans=0
+    var i = 0
+    do {
+         ans = prompt("Enter Your Guess Here..")
+
+    } while (a ==! ans) {
+        i++
+        
+        if (isNaN(a) && ans>=9) {//check nhi hui
+            console.log("You Enter Wrong!!");
+        }
+        else if (a >= 0) {// direct ispe aa gaya pc
+             ans = prompt("opps Try Again")
+        }
+        i = i + 1
+    }
+}
+
+
+
+
+
+
