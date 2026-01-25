1ab818e5f7ef3b73285e234a4e92202d4db376c0
Laptop <ma8626564@gmail.com>
2026-01-25 21:58:43 +0530

    Sorting Algorithim is not done yet its 10pm Good night!!

---
+++ 9.js
@@ -0,0 +1,31 @@
+//----------------------------------Sorting Algorithms---------------------------------------|
+// . What Is Sorting?
+
+// Sorting is the process of arranging data in a specific order:
+
+// Ascending (small → large)
+
+// Descending (large → small)
+
+// Why Sorting Is Important
+
+// Enables binary search
+
+// Improves efficiency of algorithms
+
+// Used in databases, ranking systems, scheduling
+
+// Many DSA problems depend on sorted data
+
+
+
+
+// 1:- Bubble sort
+let arr=[10,5,12,1,3]
+let n = arr.lenght
+for (let i = 0; i <n-1; i++) {
+    for (let j = 0; j < array.length; j++) {
+        
+        
+    }
+   }
