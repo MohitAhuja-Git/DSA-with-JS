23fa1c016f4428f60cba65c83a4562ac999e7533
Laptop <ma8626564@gmail.com>
2026-01-26 13:04:41 +0530

    Finally Bubble Sorting Algorithm Smaj aa gaya next Selection Sort

--- 9.js
+++ 9.js
@@ -20,12 +20,17 @@
 
 
 
-// 1:- Bubble sort
-let arr=[10,5,12,1,3]
-let n = arr.lenght
-for (let i = 0; i <n-1; i++) {
-    for (let j = 0; j < array.length; j++) {
-        
-        
-    }
-   }
+// 1:- Bubble sort :-Side-by-side elements ko compare karo, agar left bada ho to swap karo.
+let arr=[10,5,12,1,3];
+let n = arr.length;
+for (let i = 0; i < n-1; i++) {
+    for (let j = 0; j < n-1-i; j++) {
+       if (arr[j]>arr[j+1]) {
+        let temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+       } 
+  }
+}
+console.log(arr)
+   
