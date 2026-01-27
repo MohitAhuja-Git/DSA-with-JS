d0784fa2dbdd98fe502a49f2ba4b0228c178492e
Laptop <ma8626564@gmail.com>
2026-01-27 17:16:28 +0530

    video 10 half done

--- 10.js
+++ 10.js
@@ -5,7 +5,8 @@
 // Recursion:-its a function who call itself again and again!
 
 // Notes:- har stack ma data store ho jaya h 
-
+// Backtrack:-When Your Functions is return in stack at this time your query of this function is execute 
+// example your function c :-firstly your function is added in stack then excecute and print
 
 function a(n) {
     if (n==0 || n<0) return;
@@ -22,14 +23,22 @@ function b(n) {
 function c(n) {
     if (n==0) return console.log(n+ 'is Reached!')
     c(n-1);
-    console.log(n)
+    console.log(n) // This line is excecute at the time of back_tracking 
 }   
 
-c(9070)// is is last limit of recalling
-    
-    
-    
-    
-    
-    
-    
+// c(9070)// is is last limit of recalling
+
+function sum(n) {
+    if (n==1) return 1;
+    return n+sum(n-1);
+}
+function fact(n) {
+    if (n==1) return 1;
+    return n*fact(n-1);
+}    
+console.log(sum(5));
+console.log(fact(5));
+
+let n=5;
+for (let i = 0; i <=n; i++) {
+}
