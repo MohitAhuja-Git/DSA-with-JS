bd5a6bc9547ec2ea3ee8bd903ecd315490577218
Laptop <ma8626564@gmail.com>
2026-01-27 19:19:43 +0530

    Recursion level 1 done

--- 10.js
+++ 10.js
@@ -7,7 +7,7 @@
 // Notes:- har stack ma data store ho jaya h 
 // Backtrack:-When Your Functions is return in stack at this time your query of this function is execute 
 // example your function c :-firstly your function is added in stack then excecute and print
-
+// recurssive tree:-
 function a(n) {
     if (n==0 || n<0) return;
     console.log("Yo")
@@ -36,9 +36,26 @@ function fact(n) {
     if (n==1) return 1;
     return n*fact(n-1);
 }    
-console.log(sum(5));
-console.log(fact(5));
+// console.log(sum(5));
+// console.log(fact(5));
+// --------------------------------------------------------
+let n=10;
+let first =0 , second =1;
 
-let n=5;
-for (let i = 0; i <=n; i++) {
+for (let i = 0; i <=n-2; i++) {
+    let third=first+second;
+    first = second;
+    second=third
+    process.stdout.write(third+" ")
 }
+
+function fibo(n,f,s) {
+    if (n==0) return 
+    let third=first+second;
+    process.stdout.write(third+" ")
+    fibo(n-1,s,third)
+    
+}
+let bb=10;
+// process.stdout.write(0+" "+1+" ");
+fibo(n-2 , 0 , 1)
